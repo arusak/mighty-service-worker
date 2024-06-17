a30b5839194426b3d38b9cd20b4ccd6554af4299
Anton Rusak <anton.rusak@perfectart.com>
2024-06-17 14:14:11 +0300

    added pinia

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -3,19 +3,19 @@
     <h1 class="text-3xl font-bold mb-4">Data List</h1>
     <ul>
       <li
-        v-for="item in dataList"
-        :key="item.id"
-        class="mb-2 p-4 bg-white rounded-lg shadow-md flex"
+          v-for="item in dataList"
+          :key="item.id"
+          class="mb-2 p-4 bg-white rounded-lg shadow-md flex"
       >
         <span class="flex-1">{{ item.note }}</span>
         <span>
-          <button @click="() => deleteNote(item.id)">Delete</button>
+          <button @click="deleteNote(item.id)">Delete</button>
         </span>
       </li>
     </ul>
     <button
-      @click="promptAddNote"
-      class="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        @click="promptAddNote"
+        class="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
     >
       Add New Note
     </button>
@@ -23,55 +23,43 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import notesService from "../services/NotesService";
-import { Note } from "../api";
+import {computed, defineComponent, onMounted} from 'vue';
+import {useNotesStore} from "../stores/NotesStore.ts";
 
 export default defineComponent({
-  data() {
-    return {
-      dataList: [] as Note[],
+  setup() {
+    const notesStore = useNotesStore();
+
+    const dataList = computed(() => notesStore.getDataList);
+
+    const fetchData = async () => {
+      await notesStore.fetchData();
     };
-  },
-  mounted() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      try {
-        this.dataList = await notesService.getAllNotes();
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    },
 
-    async addNote(noteText: string) {
-      try {
-        const newNote = await notesService.createNote(noteText);
-        this.dataList.push(newNote);
-      } catch (error) {
-        console.error("Error adding note:", error);
-      }
-    },
+    const addNote = async (noteText: string) => {
+      await notesStore.addNote(noteText);
+    };
 
-    async deleteNote(id: string) {
-      try {
-        await notesService.deleteNote(id);
-        const idx = this.dataList.findIndex((n) => n.id === id);
-        if (idx !== -1) {
-          this.dataList.splice(idx, 1);
-        }
-      } catch (error) {
-        console.error("Error deleting note:", error);
-      }
-    },
+    const deleteNote = async (id: string) => {
+      await notesStore.deleteNote(id);
+    };
 
-    promptAddNote() {
+    const promptAddNote = () => {
       const noteText = prompt("Enter the new note:");
       if (noteText) {
-        this.addNote(noteText);
+        addNote(noteText);
       }
-    },
+    };
+
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      dataList,
+      promptAddNote,
+      deleteNote,
+    };
   },
 });
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+import pathsPlugin from "vite-tsconfig-paths";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 import { readFileSync } from "fs";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import {manifest} from "./config/manifest.config";
-import {workbox} from "./config/workbox.config";
+import { manifest } from "./config/manifest.config";
+import { workbox } from "./config/workbox.config";
 
 export default defineConfig(({ mode }) => {
   const { SSL_PRIVATE_KEY_PATH, SSL_PUBLIC_KEY_PATH } = loadEnv(
@@ -17,11 +18,14 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
+      pathsPlugin(),
       vue(),
-      VitePWA({ registerType: "autoUpdate",
+      VitePWA({
+        registerType: "autoUpdate",
         manifest,
         workbox,
-        devOptions: { enabled: true } }),
+        devOptions: { enabled: true },
+      }),
     ],
     server: {
       port: 3000,
